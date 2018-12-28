c3c96128731d1069bf3e330e310e3c5a43d1fe1b
Billi Ramirez <ramirezbilly42@outlook.com>
2018-12-28 14:53:00 -0600

    using magic associations methods

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,12 +5,15 @@ const description = "this is a good phone";
 const urlAvatar = "http://www.google.com?lkajlfjalfkfkaasfa.png";
 const price = 32;
 // Let's create a new Product
-Product.create({
-    title: title,
-    price: price,
-    urlAvatar: urlAvatar,
-    description: description
-}).then(res => console.log(res)).catch(err => console.log(err));
+
+    req.user.createProduct({
+        title: title,
+        price: price,
+        urlAvatar: urlAvatar,
+        description: description
+    })
+        .then(res => console.log(res))
+        .catch(err => console.log(err));
 
 // Retrieving data
 
