bef29b3290c68cf7836c9edf124addd662a8e96a
Billi Ramirez <ramirezbilly42@outlook.com>
2018-12-28 11:25:28 -0600

    fetching a single product

--- controllers/admin.js
+++ controllers/admin.js
@@ -12,3 +12,30 @@ Product.create({
     description: description
 }).then(res => console.log(res)).catch(err => console.log(err));
 
+// Retrieving data
+
+Product.findAll()
+    .then(products => console.log(products))
+    .catch(err =>  console.log(err));
+
+    
+
+// Retreive a single record
+
+Product.findById(prodId)
+    .then(product => console.log(product))
+    .catch(err => console.log(err));
+
+// alternative, this return an array
+
+Product.findAll({where: { id: prodId }})
+    .then(products => {
+        res.render('shop/product-detail', {
+            product: products[0],
+            title: products[0].title,
+            path: '/products'
+        })
+    })
+    .catch(err => console.log(err));
+
+
