a956539611c08d8724aa1cbd21801cf50399a7fc
Billi Ramirez <ramirezbilly42@outlook.com>
2018-12-28 14:20:59 -0600

    creating a one-to-many relation

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 const sequelize = require('./database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
+
+// RELATE THE ENTITIES
+
+ Product.belongsTo(User, {
+     constraints: true,
+     onDelete: 'CASCADE'
+ });
+ User.hasMany(Product);
 
 sequelize
-    .sync()//this sync create the tables
+    .sync({force: true})//this sync create the tables
     .then(result => console.log(result))// if the creation went well lets console the result
     .catch(err => console.log(err)); //throw an error
